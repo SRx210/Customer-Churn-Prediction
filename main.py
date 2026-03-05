c2c105e3461a68488ce9fda4749cd14cc499b6ea
SRx210 <sohandr9090@gmail.com>
2026-03-05 08:10:16 +0530

    Update: main and req files

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 
 app = Flask(__name__)
 
-MODEL_PATH = 'exports\customer_churn_model.pkl'
-ENCODER_PATH = 'exports\encoders.pkl'
+MODEL_PATH = 'exports/customer_churn_model.pkl'
+ENCODER_PATH = 'exports/encoders.pkl'
 
 loaded_model = None
 encoders = None
@@ -78,4 +78,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
